e274fd839b329b23b46cda61f976bb2cfd7e9389
VladVancea <vlad0712@gmail.com>
2019-05-29 21:59:30 +0300

    Objects injection

---
+++ spring.basic.demo/src/spring/basic/demo/shapes/Point.java
@@ -0,0 +1,22 @@
+package spring.basic.demo.shapes;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
--- spring.basic.demo/src/spring/basic/demo/shapes/Triangle.java
+++ spring.basic.demo/src/spring/basic/demo/shapes/Triangle.java
@@ -1,28 +1,37 @@
 package spring.basic.demo.shapes;
 
 public class Triangle {
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
-    private String type;
-    private int height;
-
-    public String getType() {
-        return type;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public int getHeight() {
-        return height;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
     }
 
-    public Triangle(String type) {
-        this.type = type;
+    public Point getPointB() {
+        return pointB;
     }
 
-    public Triangle(String type, int height) {
-        this.type = type;
-        this.height = height;
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     public void draw() {
-        System.out.println("Drawn " + getType() + " triangle of height " + getHeight());
+        System.out.println("(" + pointA.getX() + "," + pointA.getY() + ")");
+        System.out.println("(" + pointB.getX() + "," + pointB.getY() + ")");
+        System.out.println("(" + pointC.getX() + "," + pointC.getY() + ")");
     }
 }
