11d38dbda27c9f9149418a6f2d75f7faa6ae23f5
VladVancea <vlad0712@gmail.com>
2019-05-29 22:14:48 +0300

    Collection injection

--- spring.basic.demo/src/spring/basic/demo/shapes/Triangle.java
+++ spring.basic.demo/src/spring/basic/demo/shapes/Triangle.java
@@ -1,37 +1,22 @@
 package spring.basic.demo.shapes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Triangle {
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    private List<Point> points = new ArrayList<Point>();
 
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
     public void draw() {
-        System.out.println("(" + pointA.getX() + "," + pointA.getY() + ")");
-        System.out.println("(" + pointB.getX() + "," + pointB.getY() + ")");
-        System.out.println("(" + pointC.getX() + "," + pointC.getY() + ")");
+        for (Point point : points) {
+            System.out.println("(" + point.getX() + "," + point.getY() + ")");
+        }
     }
 }
